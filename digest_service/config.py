01f0d245a1d9c09da1050fb0c58a70ec06a56f35
Arseniks <senya.korotkov@gmail.com>
2023-07-22 00:55:19 +0300

    Refactoring and adding declarative_base

--- digest_service/config.py
+++ digest_service/config.py
@@ -8,7 +8,10 @@ load_dotenv()
 
 
 class AppSettings(BaseSettings):
-    DB_CONNECTION_URI: str = os.environ.get("DB_CONNECTION_URI", "postgresql://postgres:postgres@localhost:5432/dev_db")
+    DB_CONNECTION_URI: str = os.environ.get(
+        "DB_CONNECTION_URI",
+        "postgresql+asyncpg://postgres:postgres@localhost:5432/dev_db",
+    )
     BACKEND_URL: str = os.environ.get("BACKEND_URL", "http://localhost")
     BACKEND_PORT: str = os.environ.get("BACKEND_PORT", "5001")
     BACKEND_URL_WITH_PORT: str = f"{BACKEND_URL}:{BACKEND_PORT}"
